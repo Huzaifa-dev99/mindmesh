b31990fd8fc2a57b543b3210da5bd8581dbb7e25
SOF-TECH-LAP-36\huzaifa <huzaifaakamhz@gmail.com>
2025-07-17 01:05:06 +0500

    MINDM-7

---
+++ frontend/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit import session_state as ss
+
+st.set_page_config(
+    "MindMesh",
+    page_icon="🧠",
+    initial_sidebar_state="auto"
+)
+
+# # # # # # # # # # # # # # 
+# # Session State Variables
+
+
+
+# # # # # # # # # # # # # # 
+
+with st.sidebar:
+    st.title("MindMesh")
+    st.subheader("Ai meshed with your Mind")
+
+    
